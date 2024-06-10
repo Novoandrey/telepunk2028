ff5168db961beb41a5ce93401668ce117c7022f3
Novoandrey <86906678+Novoandrey@users.noreply.github.com>
2024-06-11 01:17:49 +0500

    Added LocalizationManager, Updated Localization
    subj

---
+++ scripts/LocalizationManager.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_button_ru_button_down():
+	TranslationServer.set_locale("ru")
+	pass # Replace with function body.
+
+
+func _on_button_en_button_down():
+	TranslationServer.set_locale("en")
+	pass # Replace with function body.
+
+
+func _on_button_fr_button_down():
+	TranslationServer.set_locale("fr")
+	pass # Replace with function body.
