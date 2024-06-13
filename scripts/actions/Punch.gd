5558d9c371e3e9cb63917e6a92684a0cfbbe4cc1
TortikP <137340296+TortikP@users.noreply.github.com>
2024-06-13 22:42:26 +0500

    Delete scripts directory

--- scripts/actions/Punch.gd
+++
@@ -1,8 +0,0 @@
-extends Action
-
-func activate(target: Critter):
-	_player.animation_tree["parameters/conditions/isPunching"] = true
-	_player._currentActionPoints -= _cost
-	target._currentHealth -= _damage
-	self.queue_free()
-	pass
--- scripts/tiles/FireTile.gd
+++
@@ -1,4 +0,0 @@
-extends Tile
-
-func deal_damage(body: Node2D):
-	body._currentHealth -= 5
