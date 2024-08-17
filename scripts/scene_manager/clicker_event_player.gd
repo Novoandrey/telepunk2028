96b1cdb9c592c4e2731ec0d6236d519b9cc00d2c
TortikP <pinjakov22@gmail.com>
2024-08-17 16:37:45 +0500

    Добавление EventController v0.0.1
    EventController является AnimationPlayer'ом. Настройка анимации отвечает за контроль flow события.
    Есть template для контроллера по пути assets/nodes/tools/event_controller

--- scripts/scene_manager/clicker_event_player.gd
+++ scripts/scene_manager/clicker_event_player.gd
@@ -1,5 +1,34 @@
+class_name ClickerEventPlayer
+
 extends AnimationPlayer
 
+@export var animation_skip_time: float = 1 ##Длительность пропуска сцены
+@export var starting_animation_name: String = "stop" ##Название анимации для воспроизведения
+@export var can_interact: bool = true ##Игрок может действовать(тапать) на сцене
+
+var is_skipping: bool = false
+
+func _ready():
+	print(get_animation_list())
+	pass
+
 func _unhandled_input(event):
+	if !can_interact:
+		return
+	
 	if event.is_action_released("ui_action"):
-		play("001_intro_animation")
+		if !is_playing(): #Запустить анимацию
+			play(starting_animation_name)
+		elif !is_skipping: #Пропуск сцены
+			speed_scale = current_animation_length / animation_skip_time
+			is_skipping = true
+		else: #Остановка пропуска сцены
+			speed_scale = 1
+			is_skipping = false
+
+func _play_until_key(): ##Остановить воспроизведения на данной точке (при пропуске сцены, данные метод игнорируется)
+	if is_skipping:
+		return
+	pause()
+
+
